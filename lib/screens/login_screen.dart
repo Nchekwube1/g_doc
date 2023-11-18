ce239f1e29629477639333853439b0fc60d84ef6
Unekwe Francis <unekwenchekwube@gmail.com>
2023-11-18 15:55:01 +0100

    setup login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:googledocsclone/colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Image.asset(
+            'assets/images/google_icon.png',
+            width: 30,
+            height: 30,
+          ),
+          label: const Text(
+            'Sign In with Google',
+            style: TextStyle(color: blackColor),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: whiteColor,
+            minimumSize: const Size(150, 50),
+          ),
+        ),
+      ),
+    );
   }
 }
