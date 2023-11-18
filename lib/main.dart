c4cf1683f89c7dc407a26abe79ac23480d42fbb0
Unekwe Francis <unekwenchekwube@gmail.com>
2023-11-18 15:25:24 +0100

    addedd login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:googledocsclone/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('Flutter Demo Home Page'),
+      home: const LoginScreen(),
     );
   }
 }
